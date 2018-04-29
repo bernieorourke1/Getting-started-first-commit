9de41703ff6bf8286c5438314ebab4e0cdb101d0
Bernadette <macuser@Bernadette.local>
2018-04-29 19:02:37 +0100

    Bernie pain in the arse commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pm-root',
+  template: `
+  <pm-products></pm-products>
+  `
+})
+export class AppComponent {
+  title = 'Angular: Getting Started';
+}
